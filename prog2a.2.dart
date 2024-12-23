45f4686ff974633f7aed8d66067ad7a6b029f14e
Mohammad Shariq Pir <pirshariq419@gmail.com>
2024-12-24 01:45:21 +0530

    first commit

--- prog2a.2.dart
+++ prog2a.2.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
   home: Scaffold(
-    appBar: AppBar(title: const Text('Insert Image Demo')),
-    body: Center(
-      child: Column(
-        children: [
+  appBar: AppBar(title: const Text('Insert Image Demo')),
+  body: Center(
+  child: Column(
+         children: [
         
-        Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnGWEwXpRS7z7rVaGrjIWWTdE8_TiYTGiYjA&s'),
+          Image.asset('assets/camera.jpg'),
 
         ],
       ),
