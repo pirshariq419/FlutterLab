e3764cc7d7fe99f2ab2be2d76933fc253ed91113
Mohammad Shariq Pir <pirshariq419@gmail.com>
2024-12-24 01:35:08 +0530

    first commit

--- prog1b.dart
+++ prog1b.dart
@@ -8,11 +8,8 @@ var firstName = "John";
 var lastName = "Doe";
 print("Full name is $firstName $lastName");
 
-print("Enter the first number:");
-int num1 = int.parse(stdin.readLineSync()!);
-
-print("Enter the second number:");
-int num2 = int.parse(stdin.readLineSync()!);
+int num1=40;
+int num2=30;  
 
 int sum = num1 + num2;
 int diff = num1 - num2;
@@ -24,9 +21,6 @@ print("The diff is $diff");
 print("The mul is $mul");
 print("The div is $div");
 
-print("Enter number:");
-int? number = int.parse(stdin.readLineSync()!);
-print("The entered number is ${number}");
 
 }
 
